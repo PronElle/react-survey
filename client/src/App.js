b723474b3f5dbb17b8875ebe72060a7f8359691b
PronElle <Massimiliano.Pronesti@gmail.com>
2021-06-09 14:07:18 +0200

    chore: restyle frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { withRouter, Switch, Route } from 'react-router-dom';
+import { AdminContext } from './AdminContext';
 
 // components
 import NavBar from './components/NavBar';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <>
-     <NavBar/>
-    </>
+    <AdminContext.Provider>
+      <Container fluid>
+        <NavBar/>
+        <Switch>
+
+          <Route path='/login'>
+            <LoginForm/>
+          </Route>
+
+          
+        </Switch>
+      </Container>
+    </AdminContext.Provider>
   );
 }
 
--- client/src/icons.js
+++ client/src/icons.js
@@ -17,4 +17,10 @@ const iconPerson = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30
                       <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z"/>
                   </svg>;
 
-export { iconDelete, iconEdit, iconLock, iconPerson };
+const iconPersonCircle = <svg className="bi bi-people-circle" width="30" height="30" viewBox="0 0 16 16" fill="orange" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z" />
+                          <path fillRule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                          <path fillRule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clipRule="evenodd" />
+                        </svg>
+
+export { iconDelete, iconEdit, iconLock, iconPerson, iconPersonCircle };
